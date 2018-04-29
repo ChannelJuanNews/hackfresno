496cdddcd6d3fcb967194c3a39003ca29e438d27
ChannelJuanNews <jruel006@ucr.edu>
2018-04-28 21:44:23 -0700

    moved index.html

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(morgan('dev'))
 
 /* Root Route */
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '/../index.html'))
+    res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 /* Upload  */
